5fcd2e84c1b5edb5b434792d1b1c59a261322261
DrEden33773 <edwardwang33773@gmail.com>
2026-05-30 04:46:45 +0800

    Suppress root hydration warnings

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,8 +30,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <QueryProvider>
           <TooltipProvider>
             {children}
